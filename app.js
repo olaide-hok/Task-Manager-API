832648b0e4c0eddfab654e04f08758d922b5e698
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-19 16:09:28 +0100

    Setup notFound middleware for routes not created.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
 
 // middleware
 app.use(express.static('./public'));
@@ -11,6 +12,8 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
