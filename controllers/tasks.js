79f9831ebe1851f15e6abe472d8b0d75e5018d6d
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-19 16:54:23 +0100

    Create and use custom asyncWrapper in controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,89 +1,66 @@
 const Task = require('../models/Tasks');
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.status(200).json({tasks});
-    } catch (error) {
-        res.status(500).json({msg: error});
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(200).json({tasks});
+});
+
+const getTask = asyncWrapper(async (req, res) => {
+    const {id: taskId} = req.params;
+    const task = await Task.findOne({_id: taskId});
+
+    if (!task) {
+        return res.status(404).json({msg: `NO task with id : ${taskId}`});
     }
-};
 
-const getTask = async (req, res) => {
-    try {
-        const {id: taskId} = req.params;
-        const task = await Task.findOne({_id: taskId});
+    res.status(200).json({task});
+});
 
-        if (!task) {
-            return res.status(404).json({msg: `NO task with id : ${taskId}`});
-        }
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({task});
+});
 
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({msg: error});
+const updateTask = asyncWrapper(async (req, res) => {
+    const {id: taskID} = req.params;
+    const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    if (!task) {
+        return res.status(404).json({msg: `No task with id : ${taskID}`});
     }
-};
 
-const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(201).json({task});
-    } catch (error) {
-        res.status(500).json({msg: error});
-    }
-};
-
-const updateTask = async (req, res) => {
-    try {
-        const {id: taskID} = req.params;
-        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
-            new: true,
-            runValidators: true,
-        });
-
-        if (!task) {
-            return res.status(404).json({msg: `No task with id : ${taskID}`});
-        }
-
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({msg: error});
-    }
-};
+    res.status(200).json({task});
+});
 
 // PUT route controller replaces as against the PATCH which adds
-const editTask = async (req, res) => {
-    try {
-        const {id: taskID} = req.params;
-        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
-            new: true,
-            runValidators: true,
-            overwrite: true,
-        });
+const editTask = asyncWrapper(async (req, res) => {
+    const {id: taskID} = req.params;
+    const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+        new: true,
+        runValidators: true,
+        overwrite: true,
+    });
 
-        if (!task) {
-            return res.status(404).json({msg: `No task with id : ${taskID}`});
-        }
-
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({msg: error});
+    if (!task) {
+        return res.status(404).json({msg: `No task with id : ${taskID}`});
     }
-};
 
-const deleteTask = async (req, res) => {
-    try {
-        const {id: taskID} = req.params;
-        const task = await Task.findOneAndDelete({_id: taskID});
+    res.status(200).json({task});
+});
 
-        if (!task) {
-            return res.status(404).json({msg: `NO task with id : ${taskID}`});
-        }
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({msg: error});
+const deleteTask = asyncWrapper(async (req, res) => {
+    const {id: taskID} = req.params;
+    const task = await Task.findOneAndDelete({_id: taskID});
+
+    if (!task) {
+        return res.status(404).json({msg: `NO task with id : ${taskID}`});
     }
-};
+    res.status(200).json({task});
+});
 
 module.exports = {
     getAllTasks,
